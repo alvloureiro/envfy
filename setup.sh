b84dee66a3544f1a7f31931aa70898a57604a5f9
Andre Loureiro <loureiro.andrew@gmail.com>
2015-01-27 23:20:09 -0400

    Copying poweline symbols configuration file to fontconfig directory
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+current_dir=`pwd`
+
 echo -e "Installing envfy on your home folder...\n"
 echo -e "Updating the package manager...\n"
 
-sudo apt-get update
+sudo apt-get update > /dev/null
 echo -e "Installing necessary packages..."
 sudo apt-get install build-essential cmake vim vim-common vim-gtk\
-                     clang exuberant-ctags git python-fontforge python-dev unzip
+                     clang exuberant-ctags git python-fontforge python-dev unzip \
+                     > /dev/null
 
 
 echo -e "Creating symbolic links for default configuration files..."
 # Linking the configuration files
-for file in `ls -A -I .git -I setup.sh -I README.md`;
+for file in `ls -A -I .git -I setup.sh -I README.md -I .config`;
 do
-    echo $PWD/$file
 
-    if [ $PWD/$file == `readlink -f $HOME/$file` ]; then
+    if [ \( $PWD/$file == ".gitconfig" \) -a \( -f $HOME/.gitconfig \) ]; then
+        echo "git already configured..."
+        continue
+    elif [ $PWD/$file == `readlink -f $HOME/$file` ]; then
         echo "skipping..."
         continue
     elif [ -e $HOME/$file ]; then
@@ -35,8 +40,18 @@ git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 echo -e "Installing powerline fonts...\n"
-POWERLINE_FONTS=$HOME/powerline-fonts
-mkdir -p $POWERLINE_FONTS
-git clone https://github.com/powerline/fonts.git $POWERLINE_FONTS
-cd $POWERLINE_FONTS
+powerline_fonts=$HOME/powerline-fonts
+mkdir -p $powerline_fonts
+git clone https://github.com/powerline/fonts.git $powerline_fonts
+cd $powerline_fonts
 ./install.sh
+
+powerline_symbols_dir=$HOME/.config/fontconfig/conf.d
+local_powerline_symbols_dir=$current_dir/.config/fontconfig/conf.d
+powerline_conf_path=$local_powerline_symbols_dir/10-powerline-symbols.conf
+
+if ! [ -d $powerline_symbols_dir ]; then
+    mkdir -p $powerline_symbols_dir
+fi
+
+cp $powerline_conf_path $powerline_symbols_dir
